100554c868914d3e8b232592ead09dccee40da64
Sergei Deryglazov <6339878-deryglazov@users.noreply.gitlab.com>
2023-04-27 14:09:43 +0300

    Fix_AuthController

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api\V1;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
